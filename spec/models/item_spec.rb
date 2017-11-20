db44f922177e419dc1db0228c012eda78b961bd3
Cess <cessmbuguar@gmail.com>
2017-11-21 01:16:12 +0300

    tests defination for todo and item models

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #Association test
+  #ensure item record belongs to a single todo record
+  it { should belong_to(:todo) }
+    
+  #Validation test
+  #ensure column name is present before save
+  it { should validate (:name) }
 end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #Association test
+  #ensure to do model has a relationship with item model
+it { should have_many(:items).dependent(:destroy) }
+
+  #Validation tests
+  #ensure columns title and created_by are present before saving
+  it { should validate :title }
+  it { should  validate created_by }
 end
