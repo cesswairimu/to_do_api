d5f8b20e609216ae6ee119fd2f2abe1ec4b3752f
Cess <cessmbuguar@gmail.com>
2017-12-06 12:40:33 +0300

    rescue from errors

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,15 +1,31 @@
 module ExceptionHandler
   extend ActiveSupport::Concern
 
+  class AuthenticationError < StandardError; end
+  class MissingToken < StandardError; end
+  class InvalidToken < StandardError; end
   included do
     rescue_from ActiveRecord::RecordNotFound do |e|
       json_response({ message: e.message }, :not_found)
     end
 
 
-    rescue_from ActiveRecord::RecordInvalid do |e|
-      json_response({ message: e.message }, :unprocessable_entity)
-    end
+    #Define custom handler
+    rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
+    rescue_from ExpectionHandler::AuthenticationError, with: :unauthorized_request
+    rescue_from ExpectionHandler::MissingToken, with: :four_twenty_two
+    rescue_from ExpectionHandler::InvalidToken, with: :four_twenty_two
+  end
+
+  private
+  
+ # JSON response with message; Status code 422 - unprocessable entity
+  def four_twenty_two(e)
+    json_response({ message: e.message }, :not_found)
+  end
+
+   # JSON response with message; Status code 401 - Unauthorized
+  def unauthorized_request(e)
+    json.response({ message: e.message }, :unauthorized )
   end
 end
-
