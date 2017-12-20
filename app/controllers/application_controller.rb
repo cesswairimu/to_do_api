6eeb89c9e7c8c4931df51499e3445f8b288e5bc3
Cess <cessmbuguar@gmail.com>
2017-12-20 23:42:38 +0300

    authorize request for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
+
+  #called before action in controller
+  before_action :authorize_request
+  attr_reader :current_user
+
+  private
+
+  #check for a valid request and return user
+  def authorize_request
+    @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
+  end
 end
+
