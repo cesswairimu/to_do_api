991dfcc4ff2a3278cf334de4eb7d45cd8f04ba0a
Cess <cessmbuguar@gmail.com>
2018-01-01 19:09:35 +0300

    version 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 
   # Name for the controllers  without affecting URI
-  scope  module: :v1, constraints:ApiVersion.new('v1, true') do
+  scope  module: :v1, constraints:ApiVersion.new('v1', true) do
     resources :todos do
       resources :items
     end
+  end
+
+  scope  module: :v2, constraints:ApiVersion.new('v2') do
+    resources :todos, only: :index
   end
 
   post 'auth/login', to: 'authentication#authenticate'
